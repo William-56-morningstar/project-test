2a6d8e23e101805cf2efab129b3e9abddb10c1de
NOTHING <120348223+NOTHING-BEN430@users.noreply.github.com>
2025-05-06 21:01:19 +0430

    Add files via upload

--- config.js
+++ config.js
@@ -78,6 +78,5 @@ ANTI_VV: process.env.ANTI_VV || "true",
 ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
 // change it to 'same' if you want to resend deleted message in same chat 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
-// make it true for auto recoding 
-ONLYOWNER: process.env.ONLYOWNER || "_*❗This Command Can Only Be Used By My Owner !*_",
+// make it true for auto recoding
 };
