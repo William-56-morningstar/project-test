b256e3888c7f01e2718fd363099299c65962d3c7
NOTHING <120348223+NOTHING-BEN430@users.noreply.github.com>
2025-05-06 19:50:02 +0430

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+require('dotenv').config();
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
