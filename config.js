29c57b5e16020dfc7331a33d2ded9dd496906af8
NOTHING <120348223+NOTHING-BEN430@users.noreply.github.com>
2025-06-07 10:24:30 +0430

    Add files via upload

--- config.js
+++ config.js
@@ -5,80 +5,44 @@ require('dotenv').config();
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-// add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-// make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-// make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY BEN-BOT 🤍*",
-// set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "true",
-// true if want welcome and goodbye msg in groups    
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
-// make anti link true,false for groups 
-ANTI_DELETE: process.env.ANTI_DELETE || "true",
-// set true false for anti delete     
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
-//Antidelete
-MENTION_REPLY: process.env.MENTION_REPLY || "false",
-// make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/6vrc2s.jpg'",
-// add custom menu and mention reply image url
-// add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "BEN-BOT",
-// add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "BEN-BOT",
-// type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
-// chose custom react emojis by yourself 
-//ANTILINK ALL SYSTEM
-PREFIX: getConfig("PREFIX") || process.env.PREFIX || ".",
-ANTIVIEW_ONCE: process.env.ANTIVIEW_ONCE || "true",
-ANTILINK_WARN: process.env.ANTILINK_WARN || "false",
-ANTILINK_KICK: process.env.ANTILINK_KICK || "false",
-ANTILINK: process.env.ANTILINK || "false",
-// automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "93744215959",
-// add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "NOTHING TTECH",
-// add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ NOTHING TEACH*",
-// add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/6vrc2s.jpg'",
-// add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> ALWAYS ONLINE *BEN-BOT*⚡",
-// add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
-// Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
-// make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words  
-MODE: getConfig("MODE") || process.env.MODE || "public",
-// make bot public-private-inbox-group 
-// make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
-// make true for send automatic voices
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-// make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-// make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false", 
-AUTO_TYPING: process.env.AUTO_TYPING || "false",   
-READ_CMD: process.env.READ_CMD || "false",
-// true if want mark commands as read 
-DEV: process.env.DEV || "93744215959",
-//replace with your whatsapp number        
-ANTI_VV: process.env.ANTI_VV || "true",
-// true for anti once view 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
-// make it true for auto recoding
+  SESSION_ID: process.env.SESSION_ID || "",
+  PREFIX: getConfig("PREFIX") || process.env.PREFIX || ".",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "93744215959",
+  OWNER_NAME: process.env.OWNER_NAME || "ɴᴏᴛʜɪɴɢ ᴛᴇᴄʜ",
+  BOT_NAME: process.env.BOT_NAME || "ʙᴇɴ-ʙᴏᴛ",
+  STICKER_NAME: process.env.STICKER_NAME || "ʙᴇɴ-ʙᴏᴛ",
+  DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴏᴛʜɪɴɢ ᴛᴇᴄʜ*",
+  MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.postimg.cc/Y2GSGtfG/IMG-20250502-WA0012-1.jpg'",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/Y2GSGtfG/IMG-20250502-WA0012-1.jpg'",
+  LIVE_MSG: process.env.LIVE_MSG || "> ᴀʟᴡᴀʏꜱ ᴏɴʟɪɴᴇ ʙᴇɴ-ʙᴏᴛ⚡",
+  DEV: process.env.DEV || "93744215959",
+  MODE: getConfig("MODE") || process.env.MODE || "public",
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+  AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*ꜱᴇᴇɴ ʏᴏᴜʀ ꜱᴛᴀᴛᴜꜱ ʙʏ ʙᴇɴ-ʙᴏᴛ 🤍*",
+  AUTO_REACT: process.env.AUTO_REACT || "false",
+  AUTO_VOICE: process.env.AUTO_VOICE || "false",
+  AUTO_STICKER: process.env.AUTO_STICKER || "false",
+  AUTO_REPLY: process.env.AUTO_REPLY || "false",
+  AUTO_TYPING: process.env.AUTO_TYPING || "false",
+  AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+  READ_MESSAGE: process.env.READ_MESSAGE || "false",
+  READ_CMD: process.env.READ_CMD || "false",
+  ANTI_LINK: process.env.ANTI_LINK || "false",
+  ANTILINK_WARN: process.env.ANTILINK_WARN || "false",
+  ANTILINK_KICK: process.env.ANTILINK_KICK || "false",
+  ANTIVIEW_ONCE: process.env.ANTIVIEW_ONCE || "true",
+  ANTI_VV: process.env.ANTI_VV || "true",
+  ANTI_DELETE: process.env.ANTI_DELETE || "true",
+  ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
+  ANTI_BAD: process.env.ANTI_BAD || "false",
+  WELCOME: process.env.WELCOME || "true",
+  ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+  MENTION_REPLY: process.env.MENTION_REPLY || "false",
+  CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+  CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍"
 };
